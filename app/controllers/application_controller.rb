56a984f2668de66b9d3fe2665174417a11c246a3
aybarburak <aybarburak@gmail.com>
2016-07-05 16:39:31 +0200

    Add cancan gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  rescue_from CanCan::AccessDenied, :with => :access_denied
+
+  private
+  def access_denied
+    flash[:alert] = 'You are not authorized to access this page'
+    redirect_to root_path
+  end
+
 end
